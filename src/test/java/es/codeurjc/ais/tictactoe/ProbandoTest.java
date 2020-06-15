54866373008b07ce8266412b71e6a7138f608fcf
DanielLorenzo <61579849+DanielLorenzo@users.noreply.github.com>
2020-06-15 02:30:53 +0200

    Add files via upload

---
+++ src/test/java/es/codeurjc/ais/tictactoe/ProbandoTest.java
@@ -0,0 +1,40 @@
+package es.codeurjc.ais.tictactoe;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ProbandoTest {
+	protected WebDriver driver;
+
+	  @BeforeAll
+	  public static void setupClass() {
+		  WebDriverManager.chromedriver().clearCache();
+		  WebDriverManager.chromedriver().driverVersion("83.0.4103.97").setup();
+		  WebApp.start();
+	  }
+
+	  @BeforeEach
+
+	  public void setupTest() {
+	   driver = new ChromeDriver();
+	  }
+
+	  @AfterEach
+	  public void teardown() {
+	   if (driver != null) {
+		   driver.quit();
+	   }
+	  }
+
+	  @Test
+
+	  public void test() {
+	   driver.get("http://www.google.es");
+	  }
+}
